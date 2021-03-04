804c2c3e771ddc17a601aebb91e144d5af7e0d43
haji-akhundov <haji.akhundov@pwc.com>
2021-03-04 22:58:13 +0100

    Solved Leap exercise

--- bash/leap/leap.sh
+++ bash/leap/leap.sh
@@ -1,24 +1,23 @@
 #!/usr/bin/env bash
 
-# The following comments should help you get started:
-# - Bash is flexible. You may use functions or write a "raw" script.
-#
-# - Complex code can be made easier to read by breaking it up
-#   into functions, however this is sometimes overkill in bash.
-#
-# - You can find links about good style and other resources
-#   for Bash in './README.md'. It came with this exercise.
-#
-#   Example:
-#   # other functions here
-#   # ...
-#   # ...
-#
-#   main () {
-#     # your main function code here
-#   }
-#
-#   # call main with all of the positional arguments
-#   main "$@"
-#
-# *** PLEASE REMOVE THESE COMMENTS BEFORE SUBMITTING YOUR SOLUTION ***
+if [[ $# -eq 0 || $# -gt 1 || ! "$1" =~ ^[0-9]+$ ]]
+  then
+    echo "Usage: leap.sh <year>"; exit 1
+fi
+
+if [[ $(($1 % 4)) -eq 0 ]]; then
+    if [[ $(($1 % 100)) -eq 0 ]]; then
+        if [[ $(($1 % 400)) -eq 0 ]]; then
+            echo "true"; exit 0
+        fi
+        echo "false"; exit 0
+    fi
+    echo "true"; exit 0
+fi
+echo "false"; exit 0
+
+# Just leaving this here for later.
+# case $1 in
+#     ''|*[!0-9]*) echo "Usage: leap.sh <year>"; exit 1 ;;
+#     #*)  ;;
+# esac
