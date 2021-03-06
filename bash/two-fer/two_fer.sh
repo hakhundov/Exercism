ed0b77ca9ad2f7c451678683b51e1bb467d58129
haji-akhundov <haji.akhundov@pwc.com>
2021-03-06 12:57:59 +0100

    Used parameter expansion for the Two Fer exercies

--- bash/two-fer/two_fer.sh
+++ bash/two-fer/two_fer.sh
@@ -1,7 +1,11 @@
 #!/usr/bin/env bash
 
-if [[ -n "$1" ]]; then
-  echo "One for $1, one for me."
-elif [[ -z "$1" ]]; then
-  echo "One for you, one for me."
-fi
+# Improved solution
+echo "One for ${1:-"you"}, one for me."
+
+# Old solution
+# if [[ -n "$1" ]]; then
+#   echo "One for $1, one for me."
+# elif [[ -z "$1" ]]; then
+#   echo "One for you, one for me."
+# fi
